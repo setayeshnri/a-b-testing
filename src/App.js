af4d3a82649153a31e890fb8d92380f15d468779
setayeshnri <setayeshn1382@gmail.com>
2024-02-20 21:24:17 +0100

    changed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="main">
+        <div className="logo">
+          <img src="logo.png" alt="logo" className='logo'/>
+        </div>
+        <div className="main-sect">
+          <h1>1000+ Users</h1>
+          <h2>Join today.</h2>
+          <button className="google">Sign up with Google</button>
+          <p style={{ marginLeft: "110px" }}>or</p>
+          <button className="create-account">Create account</button>
+          <div className="sect2">
+            <h3>Already have an account?</h3>
+            <button className="signIn">Sign in</button>
+            <p className="terms">
+              By signing up, you agree to the Terms of Service and <br />Privacy
+              Policy, including Cookie Use.
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
   );
+   
+  
 }
 
 export default App;
