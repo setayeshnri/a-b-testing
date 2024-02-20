ad1d4f74870b1c889e09b394f25fbac9edeb6df9
setayeshnri <setayeshn1382@gmail.com>
2024-02-21 00:24:06 +0100

    .

--- src/App.js
+++ src/App.js
@@ -1,34 +1,55 @@
 import "./App.css";
 import { useEffect, useState } from "react";
 
-
-
 function App() {
   const [visitor, setVisitor] = useState(0);
   const [gdprVisible, setGdprVisible] = useState(true);
+  const randomNumber = Math.floor(Math.random() * 1000);
+  const userId = localStorage.getItem("userId");
+  const rejected = localStorage.getItem("rejected");
   useEffect(() => {
     const randomizeVisitor = Math.random();
     if (randomizeVisitor > 0.5) {
       setVisitor(1);
     }
   }, []);
-  const handleClick1 = () => {
-    console.log({
-      event: "ctaClick",
-      cta: "variantA",
-      location: "signUpPage",
-    });
-  };
-  const handleClick2 = () => {
-    console.log({
-      event: "ctaClick",
-      cta: "variantB",
-      location: "signUpPage",
-    });
-  };
-  const handleRemove = () => {
-   setGdprVisible(false);
+  let handleClick1 = () => {};
+  let handleClick2 = () => {};
+  if (!rejected) {
+  
+    handleClick1 = () => {
+      console.log({
+        userId,
+        event: "create-account-clicked",
+        cta: "variantA",
+        location: "signUpPage",
+      });
+    };
+    handleClick2 = () => {
+      localStorage.getItem("userId");
+      console.log({
+        userId,
+        event: "create-account-clicked",
+        cta: "variantB",
+        location: "signUpPage",
+      });
+    };
   }
+
+  const handleRemove = () => {
+    setGdprVisible(false);
+  };
+  const handleAccept = () => {
+    localStorage.clear();
+    handleRemove();
+    localStorage.setItem("userId", randomNumber.toString());
+  };
+  const handleReject = () => {
+    localStorage.clear();
+    localStorage.setItem("rejected", setVisitor());
+
+    handleRemove();
+  };
   return (
     <>
       <div className="main">
@@ -56,27 +77,28 @@ function App() {
             <button className="signIn">Sign in</button>
             <p className="terms">
               By signing up, you agree to the Terms of Service and <br />
-              Privacy Policy, including Cookie Use.
+              Privacy Policy, including <a href="">Cookie Use.</a>
             </p>
           </div>
         </div>
       </div>
-      {gdprVisible &&(
-      <div className="gdpr">
-        <div className="cookies">
-          <h4>Did someone say ... cookies?</h4>
-          <p>
-            X and its partners use cookies to provide you with a better, safer
-            and faster service and to support our business. Some cookies are
-            necessary to use our services, improve our services, and make sure
-            they work properly.
-          </p>
+      {gdprVisible && (
+        <div className="gdpr">
+          <div className="cookies">
+            <h4>Did someone say ... cookies?</h4>
+            <p>
+              S and its partners use cookies to provide you with a better, safer
+              and faster service and to support our business. Some cookies are
+              necessary to use our services, improve our services, and make sure
+              they work properly.
+            </p>
+          </div>
+          <div className="buttons" style={{ marginTop: "5px" }}>
+            <button onClick={handleAccept}>Accept all cookies</button>
+            <button onClick={handleReject}>Refuse non-essential cookies</button>
+          </div>
         </div>
-        <div className="buttons" style={{ marginTop: "5px" }}>
-          <button onClick={handleRemove}>Accept all cookies</button>
-          <button onClick={handleRemove}>Refuse non-essential cookies</button>
-        </div>
-      </div>)}
+      )}
     </>
   );
 }
